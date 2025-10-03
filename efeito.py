d4e080a42482ec9ebfed5efb96fc1c365c8b9e10
gustavo-p-glitch <gtoso8985@gmail.com>
2025-10-02 21:53:54 -0300

    Create efeito.py

---
+++ efeito.py
@@ -0,0 +1,9 @@
+class Efeito:
+    def __init__(self, dano=0, cura=0, aumentar_ataque=0, aumentar_defesa=0, congelamento=False, fogo_dano=0, fogo_duracao=0):
+        self.dano = dano
+        self.cura = cura
+        self.aumentar_ataque = aumentar_ataque
+        self.aumentar_defesa = aumentar_defesa
+        self.congelamento = congelamento
+        self.fogo_dano = fogo_dano
+        self.fogo_duracao = fogo_duracao
