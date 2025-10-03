cd9cb88a80fb2a4db0bd0f2373a7d9025d712171
gustavo-p-glitch <gtoso8985@gmail.com>
2025-10-02 21:50:41 -0300

    Update habilidade.py

--- habilidade.py
+++ habilidade.py
@@ -1,5 +1,8 @@
+from tipo import Tipo
+from efeito import Efeito
+
 class Habilidade:
-    def __init__(self, nome: str, tipo: str, customagia: int, efeitos: dict):
+    def __init__(self, nome: str, tipo: Tipo, customagia: int, efeitos: Efeito):
         self.__nome = nome
         self.__tipo = tipo
         self.__customagia = customagia
@@ -24,15 +27,24 @@ class Habilidade:
     def usar(self, jogadorusando, alvo):
         if jogadorusando.magia >= self.customagia:
             jogadorusando.magia -= self.customagia
-
             print(f"{jogadorusando.nome} usa {self.nome}!")
 
-            if self.tipo == 'ataque' and 'dano' in self.efeitos:
-                dano = self.efeitos['dano']
-                alvo.leveidano(dano)
-            elif self.tipo == 'cura' and 'cura' in self.efeitos:
-                cura = self.efeitos['cura']
-                alvo.vida += cura
-                print(f"{alvo.nome} foi curado em {cura} pontos de vida!")
+            if self.efeitos.dano > 0:
+                alvo.leveidano(self.efeitos.dano)
+
+            if self.efeitos.cura > 0:
+                alvo.vida += self.efeitos.cura
+                print(f"{alvo.nome} foi curado em {self.efeitos.cura} pontos de vida!")
+
+            if self.efeitos.aumentar_ataque > 0:
+                jogadorusando.atk += self.efeitos.aumentar_ataque
+                print(f"O ataque de {jogadorusando.nome} aumentou em {self.efeitos.aumentar_ataque}!")
+
+            if self.efeitos.fogo_dano > 0 and self.efeitos.fogo_duracao > 0:
+                alvo.aplicar_fogo(self.efeitos.fogo_dano, self.efeitos.fogo_duracao)
+
+            if self.efeitos.congelamento:
+                alvo.aplicar_congelamento()
+
         else:
             print(f"{jogadorusando.nome} não tem magia suficiente para usar {self.nome}.")
